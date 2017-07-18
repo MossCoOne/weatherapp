a6a1aea6e763cf8a4796017354597354645c8b0d
MMoabelo <mosscomoabelo@gmail.com>
2017-07-18 17:49:48 +0200

    Add menu item

--- MyWeatherApp/app/src/main/java/za/co/mossco/myweatherapp/weather/MainActivity.java
+++ MyWeatherApp/app/src/main/java/za/co/mossco/myweatherapp/weather/MainActivity.java
@@ -3,6 +3,7 @@ package za.co.mossco.myweatherapp.weather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import za.co.mossco.myweatherapp.R;
 
@@ -17,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
         mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(mainToolbar);
         getSupportActionBar().setTitle(getString(R.string.app_name));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
