c2ad1d0cf4df854ff8f24e8b7ffeeaea4fa039db
MMoabelo <mosscomoabelo@gmail.com>
2017-08-22 14:11:14 +0200

    Add the Contants class to hold all the constant variable

--- MyWeatherApp/app/src/main/java/za/co/mossco/myweatherapp/utility/Constants.java
+++ MyWeatherApp/app/src/main/java/za/co/mossco/myweatherapp/utility/Constants.java
@@ -1,8 +1,7 @@
 package za.co.mossco.myweatherapp.utility;
 
-/**
- * Created by MMoabelo on 2017/08/22.
- */
 
 public class Constants {
+    public static final   String APP_ID = "a437f7c297a6555658f7a77abc144233";
+    public static String OPEN_WEATHER_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/";
 }
