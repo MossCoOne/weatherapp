6b3884fc4c6f2fb040c527d84f9bec069d9e44b1
mosesmoabelo <kgoputsomoses.moabelo@absa.co.za>
2018-04-10 16:09:43 +0200

    Pass the the location from the server

--- MyWeatherApp/app/src/main/java/za/co/mossco/myweatherapp/weatherdetail/WeatherDetailActivity.java
+++ MyWeatherApp/app/src/main/java/za/co/mossco/myweatherapp/weatherdetail/WeatherDetailActivity.java
@@ -2,9 +2,9 @@ package za.co.mossco.myweatherapp.weatherdetail;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,17 +16,18 @@ import java.util.Objects;
 import za.co.mossco.myweatherapp.R;
 import za.co.mossco.myweatherapp.utility.Constants;
 import za.co.mossco.myweatherapp.utility.DateUtil;
-import za.co.mossco.myweatherapp.weather.WeatherFragment;
 
 public class WeatherDetailActivity extends AppCompatActivity {
 
-    private static String currentWeather = "CurrentWeather";
+    private static String currentWeather = "current_weather";
+    private static String currentLocation = "current_location";
     za.co.mossco.myweatherapp.model.bean.List currentWeatherSeleted;
 
-    public static Intent getInstance(Context context, za.co.mossco.myweatherapp.model.bean.List list) {
+    public static Intent getInstance(Context context, za.co.mossco.myweatherapp.model.bean.List list, String location) {
         Intent detailIntent = new Intent(context, WeatherDetailActivity.class);
         Bundle bundle = new Bundle();
         bundle.putString(currentWeather, new Gson().toJson(list));
+        bundle.putString(currentLocation, location);
         detailIntent.putExtras(bundle);
         return detailIntent;
     }
@@ -46,14 +47,15 @@ public class WeatherDetailActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(getString(R.string.daily_weather));
         String jsonWeather = getIntent().getStringExtra(currentWeather);
-        initializeUI(jsonWeather);
+        String location = getIntent().getStringExtra(currentLocation);
+        initializeUI(jsonWeather, location);
     }
 
 
-    void initializeUI(String jsonWeather) {
+    void initializeUI(String jsonWeather, String location) {
         currentWeatherSeleted = new Gson().fromJson(jsonWeather, za.co.mossco.myweatherapp.model.bean.List.class);
         TextView detailDay = findViewById(R.id.detail_day);
-        detailDay.setText(String.format("%s - %s", DateUtil.getCurrentDayOfWeek(currentWeatherSeleted.getDt()), WeatherFragment.LOCATION));
+        detailDay.setText(String.format("%s - %s", DateUtil.getCurrentDayOfWeek(currentWeatherSeleted.getDt()), location));
         TextView detailDate = findViewById(R.id.detail_date);
         detailDate.setText(DateUtil.getDayAndMonth(currentWeatherSeleted.getDt()));
         TextView detailHighTemperature = findViewById(R.id.detail_temperature_high);
