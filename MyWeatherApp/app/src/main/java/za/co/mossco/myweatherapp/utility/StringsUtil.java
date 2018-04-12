0e994b82232a618258f6520578bfc7a5979787f1
mosesmoabelo <kgoputsomoses.moabelo@absa.co.za>
2018-04-13 01:37:29 +0200

    Add a method to build icon url

---
+++ MyWeatherApp/app/src/main/java/za/co/mossco/myweatherapp/utility/StringsUtil.java
@@ -0,0 +1,7 @@
+package za.co.mossco.myweatherapp.utility;
+
+public class StringsUtil {
+    public static String getIconUrl(String iconName) {
+        return "http://openweathermap.org/img/w/" + iconName + ".png";
+    }
+}
